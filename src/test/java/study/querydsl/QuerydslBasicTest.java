6caabaff357a0b68f582aeeb0ef539c1e25266f9
qkrxodud00 <qkrxodud00@gmail.com>
2022-05-13 17:03:05 +0900

    *페치 조인 사용방법 & TEST

--- src/test/java/study/querydsl/QuerydslBasicTest.java
+++ src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -18,6 +18,8 @@ import study.querydsl.entitiy.QTeam;
 import study.querydsl.entitiy.Team;
 
 import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.PersistenceUnit;
 
 import java.util.List;
 
@@ -302,8 +304,38 @@ public class QuerydslBasicTest {
         for (Tuple tuple : fetch) {
             System.out.println("t = " + tuple);
         }
+    }
 
+    @PersistenceUnit
+    EntityManagerFactory emf;
 
+    @Test
+    public void fetchJoinNo() throws Exception {
+        em.flush();
+        em.clear();
+
+        Member findMember = queryFactory
+                .selectFrom(member)
+                .where(member.username.eq("member1"))
+                .fetchOne();
+
+        boolean loaded = emf.getPersistenceUnitUtil().isLoaded(findMember.getTeam());
+        assertThat(loaded).as("폐치 조인 미적용").isFalse();
+    }
+
+    @Test
+    public void fetchJoinuse() throws Exception {
+        em.flush();
+        em.clear();
+
+        Member findMember = queryFactory
+                .selectFrom(member)
+                .join(member.team, team).fetchJoin()
+                .where(member.username.eq("member1"))
+                .fetchOne();
+
+        boolean loaded = emf.getPersistenceUnitUtil().isLoaded(findMember.getTeam());
+        assertThat(loaded).as("폐치 조인 미적용").isTrue();
     }
 
 
